5b428b3d66fdd6b8a00a7a7ff9c3cfc088f80246
Bilyana Ninova <bilyananinova@gmail.com>
2022-03-18 22:15:36 +0200

    add lab

--- Modules and Routing/forum/src/app/app-routing.module.ts
+++ Modules and Routing/forum/src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './user/login/login.component';
 import { NewThemeComponent } from './new-theme/new-theme.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ProfileComponent } from './user/profile/profile.component';
-import { RegisterComponent } from './user/register/register.component';
 import { ThemeComponent } from './theme/theme.component';
 import { ThemeContentComponent } from './theme-content/theme-content.component';
 
@@ -14,14 +10,6 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: HomeComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
   },
   {
     path: 'themes',
@@ -36,17 +24,9 @@ const routes: Routes = [
     component: NewThemeComponent,
   },
   {
-    path: 'profile',
-    component: ProfileComponent,
-  },
-  {
     path: '**',
     component: NotFoundComponent,
   }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes)
--- Modules and Routing/forum/src/app/app.module.ts
+++ Modules and Routing/forum/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from './user/user.module';
 
 import { AppComponent } from './app.component';
 import { AsideComponent } from './aside/aside.component';
@@ -14,10 +15,7 @@ import { UserService } from './user/user.service';
 import { ThemeService } from './theme/theme.service';
 import { PostService } from './aside/post.service';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
 import { NewThemeComponent } from './new-theme/new-theme.component';
-import { ProfileComponent } from './user/profile/profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ThemeContentComponent } from './theme-content/theme-content.component';
 
@@ -31,17 +29,15 @@ import { ThemeContentComponent } from './theme-content/theme-content.component';
     ThemeComponent,
     ThemeItemComponent,
     HomeComponent,
-    LoginComponent,
-    RegisterComponent,
     NewThemeComponent,
-    ProfileComponent,
     NotFoundComponent,
     ThemeContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    UserModule,
   ],
   providers: [UserService, ThemeService, PostService],
   bootstrap: [AppComponent, HeaderComponent, FooterComponent]
